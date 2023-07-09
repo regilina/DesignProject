f547d371843e9f961fbcf3806ef914615571e770
Regi <gt12139@gmail.com>
2023-07-10 00:55:56 +0300

    Код стайл применен.

--- script.js
+++ script.js
@@ -35,12 +35,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const img = document.querySelector('.completed-projects__img-desktop')
 
   const setEntity = (index) => {
-    console.log(entities)
-    console.log('index')
-    console.log(index)
-    console.log(entities[index])
     img.style.backgroundImage = `url(${entities[index].img})`
-    console.log(img)
+  }
+
+  const setDetails = index => {
+    details[0].innerHTML = entities[index].details.city
+    details[1].textContent = entities[index].details.repairTime
+    details[2].textContent = entities[index].details.apartmentArea
+    details[3].textContent = entities[index].details.repairCost
   }
 
   const prev = document.querySelector('.prev')
@@ -53,26 +55,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
   prev.addEventListener('click', () => {
     setEntity(currentIndex === 0 ? entities.length - 1 : currentIndex - 1)
-    details[0].innerHTML = entities[currentIndex === 0 ? entities.length - 1 : currentIndex - 1].details.city
-    details[1].textContent = entities[currentIndex === 0 ? entities.length - 1 : currentIndex - 1].details.repairTime
-    details[2].textContent = entities[currentIndex === 0 ? entities.length - 1 : currentIndex - 1].details.apartmentArea
-    details[3].textContent = entities[currentIndex === 0 ? entities.length - 1 : currentIndex - 1].details.repairCost
-
+    setDetails(currentIndex === 0 ? entities.length - 1 : currentIndex - 1)
     circles[currentIndex].classList.remove('active')
     listItems[currentIndex].classList.remove('list-item_active')
     currentIndex = currentIndex === 0 ? entities.length - 1 : currentIndex - 1
-    console.log(circles[currentIndex - 1])
     circles[currentIndex].classList.add('active')
     listItems[currentIndex].classList.add('list-item_active')
   })
   next.addEventListener('click', () => {
     setEntity(currentIndex === entities.length - 1 ? 0 : currentIndex + 1)
-
-    details[0].innerHTML = entities[currentIndex === entities.length - 1 ? 0 : currentIndex + 1].details.city
-    details[1].textContent = entities[currentIndex === entities.length - 1 ? 0 : currentIndex + 1].details.repairTime
-    details[2].textContent = entities[currentIndex === entities.length - 1 ? 0 : currentIndex + 1].details.apartmentArea
-    details[3].textContent = entities[currentIndex === entities.length - 1 ? 0 : currentIndex + 1].details.repairCost
-
+    setDetails(currentIndex === entities.length - 1 ? 0 : currentIndex + 1)
     circles[currentIndex].classList.remove('active')
     listItems[currentIndex].classList.remove('list-item_active')
     currentIndex = currentIndex === entities.length - 1 ? 0 : currentIndex + 1
@@ -83,12 +75,7 @@ document.addEventListener('DOMContentLoaded', () => {
   listItems.forEach((item, index) => {
     item.addEventListener('click', () => {
       setEntity(index)
-
-      details[0].innerHTML = entities[index].details.city
-      details[1].textContent = entities[index].details.repairTime
-      details[2].textContent = entities[index].details.apartmentArea
-      details[3].textContent = entities[index].details.repairCost
-
+      setDetails(index)
       circles[currentIndex].classList.remove('active')
       listItems[currentIndex].classList.remove('list-item_active')
       currentIndex = index
@@ -100,12 +87,7 @@ document.addEventListener('DOMContentLoaded', () => {
   circles.forEach((item, index) => {
     item.addEventListener('click', () => {
       setEntity(index)
-
-      details[0].innerHTML = entities[index].details.city
-      details[1].textContent = entities[index].details.repairTime
-      details[2].textContent = entities[index].details.apartmentArea
-      details[3].textContent = entities[index].details.repairCost
-
+      setDetails(index)
       circles[currentIndex].classList.remove('active')
       listItems[currentIndex].classList.remove('list-item_active')
       currentIndex = index
